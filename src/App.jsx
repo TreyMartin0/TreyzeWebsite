3c61e14909acaed2f3e12e807386d199ab0c6950
TreyMartin0 <tmarti90@calpoly.edu>
2025-08-01 15:08:37 -0400

    experience portion complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import NavBar from './components/NavBar.jsx';
 import Hero from './sections/Hero.jsx';
 import LogoSection from './components/logoSection.jsx';
+import FeatureCards from './sections/FeatureCards.jsx';
+import Experience from './sections/Experience.jsx';
 
 const App = () => {
   return (
@@ -8,6 +10,8 @@ const App = () => {
       <NavBar />
       <Hero />
       <LogoSection />
+      <FeatureCards />
+      <Experience />
     </>
   );
 };
--- src/constants/index.js
+++ src/constants/index.js
@@ -43,19 +43,19 @@ const logoIconsList = [
 
 const abilities = [
   {
-    imgPath: '/images/seo.png',
-    title: 'Quality Focus',
-    desc: 'Delivering high-quality results while maintaining attention to every detail.',
+    imgPath: '/images/adaptation.png',
+    title: 'Adaptability',
+    desc: 'Quickly adjustings to new technologies, requirements, and evolving project needs.',
   },
   {
     imgPath: '/images/chat.png',
-    title: 'Reliable Communication',
-    desc: 'Keeping you updated at every step to ensure transparency and clarity.',
+    title: 'Effective Communication',
+    desc: 'Communicating business needs into a technical description for implementation.',
   },
   {
-    imgPath: '/images/time.png',
-    title: 'On-Time Delivery',
-    desc: 'Making sure projects are completed on schedule, with quality & attention to detail.',
+    imgPath: '/images/data-analytics.png',
+    title: 'Analytical Skills',
+    desc: 'Evaluating data, workflows, and system performance to optimize applications.',
   },
 ];
 
@@ -117,42 +117,22 @@ const techStackIcons = [
 
 const expCards = [
   {
-    review:
-      'Adrian brought creativity and technical expertise to the team, significantly improving our frontend performance. His work has been invaluable in delivering faster experiences.',
-    imgPath: '/images/exp1.png',
-    logoPath: '/images/logo1.png',
-    title: 'Frontend Developer',
-    date: 'January 2023 - Present',
+    imgPath: '/images/logos/Citynet_Logo.png',
+    title: 'Salesforce Developer Intern',
+    date: 'June 2025 - Present',
     responsibilities: [
-      'Developed and maintained user-facing features for the Hostinger website.',
-      'Collaborated closely with UI/UX designers to ensure seamless user experiences.',
-      'Optimized web applications for maximum speed and scalability.',
+      'Integrated Citynet’s core legacy systems into Salesforce to streamline operations.',
+      'Improved data access and support cross-functional reporting.',
+      'Implemented Work-Relay to optimize GigOrder workflows, enhance installation documentation, and increase task visibility across teams.',
     ],
   },
   {
-    review:
-      "Adrian’s contributions to Docker's web applications have been outstanding. He approaches challenges with a problem-solving mindset.",
-    imgPath: '/images/exp2.png',
-    logoPath: '/images/logo2.png',
-    title: 'Full Stack Developer',
-    date: 'June 2020 - December 2023',
+    imgPath: '/images/logos/cal-poly-san-luis-obispo.png',
+    title: 'Instructional Student Assistant',
+    date: 'September 2024 - June 2025',
     responsibilities: [
-      "Led the development of Docker's web applications, focusing on scalability.",
-      'Worked with backend engineers to integrate APIs seamlessly with the frontend.',
-      'Contributed to open-source projects that were used with the Docker ecosystem.',
-    ],
-  },
-  {
-    review:
-      'Adrian’s work on Appwrite’s mobile app brought a high level of quality and efficiency. He delivered solutions that enhanced our mobile experience & meet our product goals.',
-    imgPath: '/images/exp3.png',
-    logoPath: '/images/logo3.png',
-    title: 'React Native Developer',
-    date: 'March 2019 - May 2020',
-    responsibilities: [
-      "Built cross-platform mobile apps using React Native, integrating with Appwrite's backend services.",
-      'Improved app performance and user experience through code optimization and testing.',
-      'Coordinated with the product team to implement features based on feedback.',
+      'Provided students with tutoring in order to understand and learn the concepts of Systems Programming using the C programming language.',
+      'Aided Cal Poly professor, Fahim Kahn and Paris Kalathas, with during his lab sections and hosted lectures in his absence.',
     ],
   },
 ];
